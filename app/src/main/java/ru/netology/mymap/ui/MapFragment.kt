65d0e90f798df18c0ba70567b2696ad135a4e4f8
Aleksey Volkov <cface@yandex.ru>
2021-05-07 16:46:17 +0300

    google map implemented

--- app/src/main/java/ru/netology/mymap/ui/MapFragment.kt
+++ app/src/main/java/ru/netology/mymap/ui/MapFragment.kt
@@ -1,16 +1,51 @@
 package ru.netology.mymap.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.coroutineScope
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.collections.MarkerManager
+import com.google.maps.android.ktx.awaitAnimateCamera
+import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.model.cameraPosition
+import com.google.maps.android.ktx.utils.collection.addMarker
+import ru.netology.mymap.R
 import ru.netology.mymap.databinding.FragmentMapBinding
 import ru.netology.mymap.viewmodel.PlacesViewModel
+import ru.netology.mymap.utils.icon
 
 class MapFragment : Fragment() {
 
     private lateinit var placesViewModel: PlacesViewModel
+
+    private lateinit var googleMap: GoogleMap
+
+    @SuppressLint("MissingPermission")
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                googleMap.apply {
+                    isMyLocationEnabled = true
+                    uiSettings.isMyLocationButtonEnabled = true
+                }
+            } else {
+                // TODO: show sorry dialog
+            }
+        }
 
     private val binding by lazy { FragmentMapBinding.inflate(layoutInflater) }
 
@@ -18,9 +53,87 @@ class MapFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
 
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        lifecycle.coroutineScope.launchWhenCreated {
+            googleMap = mapFragment.awaitMap().apply {
+                isTrafficEnabled = true
+                isBuildingsEnabled = true
+
+                uiSettings.apply {
+                    isZoomControlsEnabled = true
+                    setAllGesturesEnabled(true)
+                }
+            }
+
+            when {
+                // 1. Проверяем есть ли уже права
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    googleMap.apply {
+                        isMyLocationEnabled = true
+                        uiSettings.isMyLocationButtonEnabled = true
+                    }
+
+                    val fusedLocationProviderClient = LocationServices
+                        .getFusedLocationProviderClient(requireActivity())
+
+                    fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                        println(it)
+                    }
+                }
+                // 2. Должны показать обоснование необходимости прав
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    // TODO: show rationale dialog
+                }
+                // 3. Запрашиваем права
+                else -> {
+                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+
+            val target = LatLng(55.751999, 37.617734)
+            val markerManager = MarkerManager(googleMap)
+            val collection: MarkerManager.Collection = markerManager.newCollection().apply {
+                addMarker {
+                    position(target)
+                    icon(
+                        AppCompatResources.getDrawable(
+                            requireContext(),
+                            R.drawable.ic_place_64
+                        )!!)
+                    title("The Moscow Kremlin")
+                }.apply {
+                    tag = "Any additional data" // Any
+                }
+            }
+            collection.setOnMarkerClickListener { marker ->
+                // TODO: work with marker
+                Toast.makeText(
+                    requireContext(),
+                    (marker.tag as String),
+                    Toast.LENGTH_LONG
+                ).show()
+                true
+            }
+
+            googleMap.awaitAnimateCamera(
+                CameraUpdateFactory.newCameraPosition(
+                    cameraPosition {
+                        target(target)
+                        zoom(15F)
+                    }
+                ))
+        }
     }
 
 }
