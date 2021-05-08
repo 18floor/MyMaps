0ffe26183d398d3de87dc2f1b39f4535c3a7ec27
Aleksey Volkov <cface@yandex.ru>
2021-05-08 21:11:37 +0300

    add PlaceAdapter for RecyclerView

--- app/src/main/java/ru/netology/mymap/adapter/PlaceAdapter.kt
+++ app/src/main/java/ru/netology/mymap/adapter/PlaceAdapter.kt
@@ -1,7 +1,12 @@
 package ru.netology.mymap.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import ru.netology.mymap.data.Place
+import ru.netology.mymap.databinding.CardPlaceBinding
 
 interface OnInteractionListener {
     fun onEdit(place: Place) {}
@@ -11,7 +16,43 @@ interface OnInteractionListener {
 
 class PlaceAdapter(
     private val onInteractionListener: OnInteractionListener
-) {
+) : ListAdapter<Place, PlaceViewHolder>(PostDiffCallback())  {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
+        val binding = CardPlaceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PlaceViewHolder(binding, onInteractionListener)
+    }
+
+    override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+        val place = getItem(position)
+        holder.bind(place)
+    }
+}
+
+class PlaceViewHolder(
+    private val binding: CardPlaceBinding,
+    private val onInteractionListener: OnInteractionListener
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(place: Place) {
+
+        binding.apply {
+            placeTitle.text = place.titlePlace
+            placeDescription.text = place.descriptionPlace
+
+            placeIcon.setOnClickListener {
+                onInteractionListener.onMapView(place)
+            }
+
+            placeView.setOnClickListener {
+                onInteractionListener.onEdit(place)
+            }
+
+            deleteButton.setOnClickListener {
+                onInteractionListener.onDelete(place)
+            }
+        }
+    }
 }
 
 class PostDiffCallback : DiffUtil.ItemCallback<Place>() {
